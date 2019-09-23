c8f672df36b1ef4aadb70bf523a36814cf8d7433
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-09-23 13:45:33 +0530

    learning objectives in dart

---
+++ Dart04/lib/main.dart
@@ -0,0 +1,33 @@
+
+
+void main(){
+  //declare the list
+  List<int>numbers=List(5);
+  //insert data to the list
+  numbers[0]=45;
+  numbers[1]=5;
+  numbers[2]=75;
+  numbers[3]=54;
+  numbers[4]=null;
+  //print list one by one
+  print(numbers[0]);
+  print(numbers[1]);
+  print(numbers[2]);
+  print(numbers[3]);
+  print(numbers[4]);
+  //update index
+  numbers[1]=48;
+  print(numbers[1]);
+  //delete index
+  numbers[2]=null;
+  print(numbers[2]);
+  print("print list");
+  for(int num in numbers){
+    print(num);
+  }
+  print("2 way");
+ // numbers.foreach((element))=>print ((element));
+  for(int i=0;i<numbers.length;i++){
+    print(numbers[i]);
+  }
+}
